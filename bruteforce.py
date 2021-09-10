e15344f0f639280049ca2f768b718a527d58c05a
atarax-dev <83333973+atarax-dev@users.noreply.github.com>
2021-09-10 14:23:20 +0200

    rename algos

--- bruteforce.py
+++ bruteforce.py
@@ -1,38 +1,50 @@
+import itertools
+
 # actions = (prix en euros, bénéfice en % après 2 ans)
 actions_data = [(20, 5), (30, 10), (50, 15), (70, 20), (60, 17), (80, 25), (22, 7), (26, 11), (48, 13), (34, 27),
                 (42, 17), (110, 9), (38, 23), (14, 1), (18, 3), (8, 8), (4, 12), (10, 14), (24, 21), (114, 18)]
-wallet1 = 500
-action_wallet1 = []
+
+stuff = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+combinaisons = []
 
 
-def get_max_benefits_action(actions_list):
+def calcul_prix_combinaison(combinaison, actions_list):
+    total_price = 0
+    for action in combinaison:
+        price = actions_list[action-1][0]
+        total_price += price
+    return total_price
+
+
+def calcul_benefice_combinaison(combinaison, actions_list):
+    total_benefits = 0
+    for action in combinaison:
+        price = actions_list[action - 1][0]
+        benefits_percent = actions_list[action - 1][1]
+        benefit = price*benefits_percent/100
+        total_benefits += benefit
+    return total_benefits
+
+
+def calcul_meilleur_invest(combinaisons_list, actions_list):
+    max_benefit_sub_500 = 0
     i = 0
-    max_benefits = 0
-    imax = 0
-    for price, benefits in actions_list:
-        if benefits > max_benefits:
-            max_benefits = benefits
-            imax = i
+    imax_benefit = 0
+    for combinaison in combinaisons_list:
+        if calcul_benefice_combinaison(combinaison, actions_list) > max_benefit_sub_500:
+            max_benefit_sub_500 = calcul_benefice_combinaison(combinaison, actions_list)
+            imax_benefit = i
         i += 1
-    return imax
+    best_combinaison = combinaisons_list[imax_benefit]
+    return best_combinaison
 
 
-def get_best_invest(actions, wallet, action_wallet):
-    bonus = 0
-    while wallet > 0 and len(actions) > 0:
-        imax_benefits = get_max_benefits_action(actions)
-        price = actions[imax_benefits][0]
-        if wallet - price >= 0:
-            wallet -= price
-            bonus += price*(actions[imax_benefits][1]/100)
-            actions.remove(actions[imax_benefits])
-            action_wallet.append(imax_benefits + 1)
-        elif wallet - price < 0 and len(actions) > 0:
-            actions.remove(actions[imax_benefits])
+for L in range(0, len(stuff)+1):
+    for subset in itertools.combinations(stuff, L):
+        if calcul_prix_combinaison(subset, actions_data) < 500:
+            combinaisons.append(subset)
 
-    print(f"Argent restant: {wallet}€")
-    print(f"Liste des actions: {action_wallet}")
-    print(f"Gain: {bonus}")
-
-
-get_best_invest(actions_data, wallet1, action_wallet1)
+best_combo = (calcul_meilleur_invest(combinaisons, actions_data))
+print(f"Meilleure combinaison : {best_combo}")
+print(f"Bénéfice : {calcul_benefice_combinaison(best_combo, actions_data)} €")
+print(f"Prix : {calcul_prix_combinaison(best_combo, actions_data)} €")
---
+++ optimized.py
@@ -0,0 +1,44 @@
+actions_data = [(20, 5, 1), (30, 10, 2), (50, 15, 3), (70, 20, 4), (60, 17, 5), (80, 25, 6), (22, 7, 7), (26, 11, 8),
+                (48, 13, 9), (34, 27, 10), (42, 17, 11), (110, 9, 12), (38, 23, 13), (14, 1, 14), (18, 3, 15),
+                (8, 8, 16), (4, 12, 17), (10, 14, 18), (24, 21, 19), (114, 18, 20)]
+wallet1 = 500
+action_wallet1 = []
+
+
+# tri par bénef/cout
+def sort_by_efficiency(actions_list):
+    # sorted_list = sorted(actions_list, key=lambda x: (x[1]/100)/x[0], reverse=True)
+    sorted_list = sorted(actions_list, key=lambda x: x[1], reverse=True)
+    return sorted_list
+
+
+def get_best_invest(actions_list, wallet, action_wallet):
+    for action in actions_list:
+        if wallet > 0 and wallet > action[0]:
+            action_wallet.append(action)
+            wallet -= action[0]
+    return action_wallet
+
+
+def calcul_prix_combinaison(combinaison):
+    total_price = 0
+    for action in combinaison:
+        price = action[0]
+        total_price += price
+    return total_price
+
+
+def calcul_benefice_combinaison(combinaison):
+    total_benefits = 0
+    for action in combinaison:
+        price = action[0]
+        benefits_percent = action[1]
+        benefit = price * benefits_percent / 100
+        total_benefits += benefit
+    return total_benefits
+
+
+best_combo = get_best_invest(sort_by_efficiency(actions_data), wallet1, action_wallet1)
+print(f"Meilleure combinaison : {best_combo}")
+print(f"Bénéfice : {calcul_benefice_combinaison(best_combo)} €")
+print(f"Prix : {calcul_prix_combinaison(best_combo)} €")
